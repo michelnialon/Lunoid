a26a338f08bc1ab6bef297e45515f6bc364db89c
michel nialon <m.nialon@laposte.net>
2016-01-11 20:19:17 +0100

    build v622d

--- app/build/generated/source/buildConfig/debug/com/nialon/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/nialon/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.nialon";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 34;
-  public static final String VERSION_NAME = "6.22c";
+  public static final int VERSION_CODE = 35;
+  public static final String VERSION_NAME = "6.22d";
 }
