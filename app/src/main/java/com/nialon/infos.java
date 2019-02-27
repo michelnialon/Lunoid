03759a3c8e17880b1bf3bb139db14009ea4514e5
Michel Nialon <m.nialon@laposte.net>
2019-02-27 22:23:53 +0100

    Suppress warnings Suppress title

--- app/src/main/java/com/nialon/infos.java
+++ app/src/main/java/com/nialon/infos.java
@@ -1,25 +1,25 @@
 package com.nialon;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.TextView;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class infos extends Activity{
 	public void onCreate(Bundle savedInstanceState) 
 	{
+		// Pour enlever la barre de titre :
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.infos);
 		
-		/** Format title */
+		/* Format title */
 		/*
         TextView title = (TextView) findViewById(android.R.id.title);
         title.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL);
         title.setTextColor(Color.YELLOW);
         title.setTextSize(20);
         */
-        
 	}
-
 }
