0baecb12cfb716c324e3a593ebe3081a085a3ff0
Michel Nialon <m.nialon@laposte.net>
2019-02-27 20:58:53 +0100

    Version 2019 - 2

--- app/src/main/java/com/nialon/PrefsActivity.java
+++ app/src/main/java/com/nialon/PrefsActivity.java
@@ -1,12 +1,19 @@
 package com.nialon;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class PrefsActivity extends PreferenceActivity {
+public class PrefsActivity extends PreferenceActivity
+{
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 	   super.onCreate(savedInstanceState);
+//	   super.getListView().setBackgroundColor(Color.BLACK);
 	   addPreferencesFromResource(R.xml.prefs);
 	}
 }
