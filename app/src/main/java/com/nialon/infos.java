33c28aa991cc14d752473a2758602b252704f757
Michel Nialon <m.nialon@laposte.net>
2020-08-09 14:37:41 +0200

    Visualisation du fichier d'aide en html

--- app/src/main/java/com/nialon/infos.java
+++ app/src/main/java/com/nialon/infos.java
@@ -2,22 +2,37 @@ package com.nialon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
 
-public class infos extends Activity{
-	public void onCreate(Bundle savedInstanceState) 
-	{
+import java.util.Locale;
+
+public class infos extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
 		// Pour enlever la barre de titre :
 		//this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		//this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.infos);
-		
-		/* Format title */
-		/*
-        TextView title = (TextView) findViewById(android.R.id.title);
-        title.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL);
-        title.setTextColor(Color.YELLOW);
-        title.setTextSize(20);
-        */
+		//setContentView(R.layout.infos);
+		setContentView(R.layout.aide);
+		WebView webView = findViewById(R.id.wvAide);
+		Log.d("locale", Locale.getDefault().getISO3Language());
+		switch (Locale.getDefault().getISO3Language()) {
+			case "eng":
+				webView.loadUrl("file:///android_asset/aide_fr.html");
+				break;
+			case "spa":
+				webView.loadUrl("file:///android_asset/aide_es.html");
+				break;
+			case "ita":
+				webView.loadUrl("file:///android_asset/aide_fr.html");
+				break;
+			case "deu":
+				webView.loadUrl("file:///android_asset/aide_de.html");
+				break;
+			default:
+				webView.loadUrl("file:///android_asset/aide_fr.html");
+				break;
+		}
 	}
 }
